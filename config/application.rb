fda87e889ee83b0db33730e83ac8c8009ebbb3a5
Tripti <triptibhangade@gmail.com>
2025-08-22 14:31:36 +0530

    Added setup of redis caching

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,14 @@ module RedisBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
+    config.cache_store = :redis_store, (ENV.fetch("REDIS_URL") rescue "redis://localhost:6379/1"), { expires_in: 5.years }
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_paths << Rails.root.join('lib')
+
 
     # Configuration for the application, engines, and railties goes here.
     #
---
+++ lib/tolk_redis_backend.rb
@@ -0,0 +1,31 @@
+# lib/tolk_redis_backend.rb
+
+class TolkRedisBackend
+  PREFIX = 'i18n'.freeze
+
+  def initialize
+    redis_url = ENV.fetch("REDIS_URL") rescue "redis://localhost:6379/1"
+    @redis = Redis.new(url: redis_url)
+  end
+
+  def with_prefix(key)
+    "#{PREFIX}.#{key}"
+  end
+
+  def [](key)
+    @redis.get(with_prefix(key)) || ""
+  end
+
+  def []=(key, value)
+    @redis.set with_prefix(key), value
+  end
+
+  def keys
+    @redis.keys with_prefix('*')
+  end
+
+  def clear_old_entries
+    keys_to_delete = keys
+    @redis.del(*keys_to_delete) unless keys_to_delete.empty?
+  end
+end
